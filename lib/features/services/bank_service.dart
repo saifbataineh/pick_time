6c2a732b8f4f484b30f4126a13139e065318e260
zaidgasan02 <zaidabualhija9@gmail.com>
2025-06-26 18:54:57 +0300

    Added assets and updated bank_service.dart

--- lib/features/services/bank_service.dart
+++ lib/features/services/bank_service.dart
@@ -12,45 +12,60 @@ class BankService extends StatelessWidget {
       body: Column(
         children: [
           CustomContainerService(
-              leading: Image.asset('assets/images/one.png'),
-              title: 'First National Bank',
-              subtitle: '4.5 • 15 min avg. wait',
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ServiceProvider()));
-              }),
+            leading: Image.asset('assets/images/one.png'),
+            title: 'First National Bank',
+            subtitle: '4.5 • 15 min avg. wait',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ServiceProvider()),
+              );
+            },
+          ),
           CustomContainerService(
-              leading: Image.asset('assets/images/tow.png'),
-              title: 'Community Trust Bank',
-              subtitle: '4.5 • 15 min avg. wait',
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ServiceProvider()));
-              }),
+            leading: Image.asset('assets/images/tow.png'),
+            title: 'Community Trust Bank',
+            subtitle: '4.5 • 15 min avg. wait',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ServiceProvider()),
+              );
+            },
+          ),
           CustomContainerService(
-              leading: Image.asset('assets/images/three.png'),
-              title: 'United Savings Bank',
-              subtitle: '4.5 • 15 min avg. wait',
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ServiceProvider()));
-              }),
+            leading: Image.asset('assets/images/three.png'),
+            title: 'United Savings Bank',
+            subtitle: '4.5 • 15 min avg. wait',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ServiceProvider()),
+              );
+            },
+          ),
           CustomContainerService(
-              leading: Image.asset('assets/images/foure.png'),
-              title: 'Regional Credit Union',
-              subtitle: '4.5 • 15 min avg. wait',
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ServiceProvider()));
-              }),
+            leading: Image.asset('assets/images/foure.png'),
+            title: 'Regional Credit Union',
+            subtitle: '4.5 • 15 min avg. wait',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ServiceProvider()),
+              );
+            },
+          ),
           CustomContainerService(
-              leading: Image.asset('assets/images/five.png'),
-              title: 'Citywide Bank',
-              subtitle: '4.5 • 15 min avg. wait',
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ServiceProvider()));
-              }),
+            leading: Image.asset('assets/images/five.png'),
+            title: 'Citywide Bank',
+            subtitle: '4.5 • 15 min avg. wait',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ServiceProvider()),
+              );
+            },
+          ),
         ],
       ),
     );
